eb4531f3fbfcc380e8109aac92d5dc8c73ec7503
EzeG92 <gim24001@byui.edu>
2025-07-17 20:43:18 -0300

    users and tasks routes/controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -13,6 +13,105 @@ const getAllTasks = async (req, res) => {
   });
 };
 
+// Function to get a task by ID
+const getTaskById = async (req, res) => {
+  //#swagger.tags = ['Tasks'];
+  const taskId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("task")
+    .find({ _id: taskId });
+  result.toArray().then((task) => {
+    if (task.length > 0) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(task[0]);
+    } else {
+      res.status(404).json({ message: "Task not found" });
+    }
+  });
+};
+
+// Create a new Task
+const createTask = async (req, res) => {
+  //#swagger.tags = ['Tasks'];
+  const task = {
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    priority: req.body.priority,
+    dueDate: req.body.dueDate,
+    userId: new ObjectId(req.body.userId),
+  };
+  const result = await mongodb.getDb().db().collection("task").insertOne(task);
+  if (result.acknowledged) {
+    res
+      .status(201)
+      .send({ message: "Task created successfully", id: result.insertedId });
+  } else {
+    res.status(500).json({ message: "Failed to create task" });
+  }
+};
+
+// Update a Task by ID
+const updateTask = async (req, res) => {
+  //#swagger.tags = ['Tasks'];
+  const taskId = new ObjectId(req.params.id);
+  const task = {
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    priority: req.body.priority,
+    dueDate: req.body.dueDate,
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("task")
+    .replaceOne({ _id: taskId }, task);
+  if (result.modifiedCount > 0) {
+    res.status(200).send({ message: "Task updated successfully" });
+  } else {
+    res.status(404).json({ message: "Task not found or no changes made" });
+  }
+};
+
+// Delete a Task by ID
+const deleteTask = async (req, res) => {
+  //#swagger.tags = ['Tasks'];
+  const taskId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("task")
+    .deleteOne({ _id: taskId });
+  if (result.deletedCount > 0) {
+    res.status(200).send({ message: "Task deleted successfully" });
+  } else {
+    res.status(404).json({ message: "Task not found" });
+  }
+};
+
+// Get tasks by user ID
+const getTasksByUserId = async (req, res) => {
+  //#swagger.tags = ['Tasks']
+  const userId = req.params.userId;
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("task")
+    .find({ userId: new ObjectId(userId) });
+  result.toArray().then((tasks) => {
+    res.setHeader("Content-Type", "application/json");
+    res.json(tasks);
+  });
+};
+
 module.exports = {
   getAllTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+  getTasksByUserId,
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,9 +5,10 @@ const taskController = require("../controllers/tasks");
 
 // // Define routes for task-related operations
 router.get("/", taskController.getAllTasks);
-// router.get("/tasks/:id", taskController.getTaskById);
-// router.post("/tasks", taskController.createTask);
-// router.put("/tasks/:id", taskController.updateTask);
-// router.delete("/tasks/:id", taskController.deleteTask);
+router.get("/:id", taskController.getTaskById);
+router.get("/user/:userId/tasks", taskController.getTasksByUserId); // get tasks by user ID
+router.post("/", taskController.createTask);
+router.put("/:id", taskController.updateTask);
+router.delete("/:id", taskController.deleteTask);
 
 module.exports = router;
